f4fe29e9376f8911d366ff1118848b1b58d1393f
IzanGual <iesvda.izamar@gmail.com>
2025-01-20 11:07:22 +0100

    Add Supabase integration4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('fetchData').addEventListener('click', async () => {
     const response = await fetch('https://czfghywpmezemmtfdiyw.supabase.co/rest/v1/example_table', {
         headers: {
             'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZmdoeXdwbWV6ZW1tdGZkaXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjU4ODcsImV4cCI6MjA1Mjk0MTg4N30.KYxp-j3NFHvlxyRzFjq6HmT4Q_s12fRriYzCsmfRQYg',
-            'Authorization': 'Bearer your-supabase-api-key'
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZmdoeXdwbWV6ZW1tdGZkaXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjU4ODcsImV4cCI6MjA1Mjk0MTg4N30.KYxp-j3NFHvlxyRzFjq6HmT4Q_s12fRriYzCsmfRQYg'
         }
     });
 
